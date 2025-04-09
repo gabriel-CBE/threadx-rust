88add6568e882af42c9cfa5fada7f52bbbad8138
Jan Jongen <jan.jongen@valtech.com>
2025-04-09 10:14:12 +0200

    Initialization on global allocator

--- threadx-rs/src/allocator.rs
+++ threadx-rs/src/allocator.rs
@@ -1,5 +1,6 @@
 use crate::error::TxError;
 use crate::tx_checked_call;
+use core::panic;
 use core::sync::atomic::AtomicBool;
 use core::{
     alloc::{GlobalAlloc, Layout},
@@ -8,7 +9,8 @@ use core::{
 };
 use num_traits::FromPrimitive;
 use threadx_sys::{
-    _tx_byte_allocate, _tx_byte_pool_create, _tx_byte_release, CHAR, TX_BYTE_POOL, TX_WAIT_FOREVER, ULONG
+    _tx_byte_allocate, _tx_byte_pool_create, _tx_byte_release, CHAR, TX_BYTE_POOL, TX_WAIT_FOREVER,
+    ULONG,
 };
 
 /// ThreadX allocator for Rust. Instantiate this struct and use it as the global allocator.
@@ -21,10 +23,9 @@ use threadx_sys::{
 
 // We use a static mut and initialize it to zero. After this we only work with raw pointers to this static mut to avoid UB by accidentally creating aliasing mut references
 static mut POOL_STRUCT: TX_BYTE_POOL = unsafe { MaybeUninit::zeroed().assume_init() };
-
+static INITIALIZED: AtomicBool = AtomicBool::new(false);
 pub struct ThreadXAllocator {
     pool_ptr: *mut TX_BYTE_POOL,
-    initialized: AtomicBool,
 }
 
 unsafe impl Sync for ThreadXAllocator {}
@@ -34,14 +35,15 @@ impl ThreadXAllocator {
         // TODO: Make this return None if already initialized
         let allocator = ThreadXAllocator {
             pool_ptr: &raw mut POOL_STRUCT,
-            initialized: AtomicBool::new(false),
         };
 
         allocator
     }
 
     pub fn initialize(&'static self, pool_memory: &'static mut [u8]) -> Result<(), TxError> {
-        // TODO: Panic if initialized twice. Check if name is not global (and not zero)
+        if INITIALIZED.load(core::sync::atomic::Ordering::Relaxed) {
+            panic!("ThreadXAllocator already initialized");
+        }
         let pool_name = c"global";
 
         let res = tx_checked_call!(_tx_byte_pool_create(
@@ -51,15 +53,14 @@ impl ThreadXAllocator {
             pool_memory.len() as ULONG
         ));
         // Set the allocator to initialized
-        self.initialized
-            .store(true, core::sync::atomic::Ordering::Release);
+        INITIALIZED.store(true, core::sync::atomic::Ordering::Release);
         res
     }
 }
 
 unsafe impl GlobalAlloc for ThreadXAllocator {
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
-        if !self.initialized.load(core::sync::atomic::Ordering::Acquire) {
+        if !INITIALIZED.load(core::sync::atomic::Ordering::Acquire) {
             panic!("Use of ThreadX allocator before it was initialized");
         }
         let mut ptr: *mut c_void = core::ptr::null_mut() as *mut c_void;
@@ -81,7 +82,6 @@ unsafe impl GlobalAlloc for ThreadXAllocator {
     }
 
     unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
-        // Safety: _tx_byte_allocate is thread safe so it is ok to use the pool_ptr ie. a pointer into the static mut struct
         tx_checked_call!(_tx_byte_release(ptr as *mut c_void)).unwrap()
     }
 }
